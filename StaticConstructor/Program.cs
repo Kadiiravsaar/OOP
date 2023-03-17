ba19d702632b7990694d8cd537d0cfb7f2036d70
kadiiravsaar <kadiiravsar@gmail.com>
2023-03-17 15:20:08 +0300

    StaticConstructor project added

---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,21 @@
+﻿// bir sınıftan nesne oluşturulurken ilk tetiklenen fonksiyon STATİC CONSTRUCTOR'DUR
+// Lakin İLLKKKKKK nesne üretilirken.....
+
+// Ctor HERRR nesne üretilirken
+// Static Ctor İLKKKKK nesne üretilirken
+
+MyClass m1 = new MyClass(); // Önce Static, Ardından normal ctor
+MyClass m2 = new MyClass(); // artık Ctor
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("Nesne her oluşturulurken tetiklenir");
+    }
+    static MyClass()
+    {
+        Console.WriteLine("Nesne İlk oluşturulurken tetiklenir");
+
+    }
+
+}
